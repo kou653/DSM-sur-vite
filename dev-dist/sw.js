8cc8313014324f25ff62bcb2f54c9066efd28314
Kouassi Roxane <rkouassi372@gmail.com>
2026-04-23 13:48:48 +0000

    sw

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.29mi6s866t8"
+    "revision": "0.h8hrrjrf948"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
