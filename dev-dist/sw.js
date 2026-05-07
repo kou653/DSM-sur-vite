7a82365eeee441d363f7ce55e04cd3012ec18dbe
Kouassi Roxane <rkouassi372@gmail.com>
2026-05-07 11:09:16 +0000

    dev actif

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.005nahbne6g"
+    "revision": "0.240ir6r0d1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
